6354c0ab7174090327658abf0aabd6da58deff5e
M. Benjamin Tankus <tankusb@gmail.com>
2021-02-09 12:27:13 -0800

    update week 6

--- DataWrangling_HW3.py
+++ DataWrangling_HW3.py
@@ -28,7 +28,7 @@ def getData():
         }
 
     recipeDict = {}
-    n = 10 # Number of recipes to pull
+    n = 250 # Number of recipes to pull
 
     for k in range(1,n):
         recipeID = random.randint(1,300000)
---
+++ midterm.py
@@ -0,0 +1,25 @@
+# Midterm project
+# Written by Ben Tankus 2-6-21
+
+#You will have three major tasks, 
+# one is to wrangle the data into a format that allows you to do work on it and load it into a database.
+    # DONE in group project
+
+
+ 
+ 
+# The next is to answer 3 questions about your data which are moderately complex and provide accompanying visualizations if appropriate (at least 3 total), 
+    #Questions: 
+        # 1. Is it worth getting more pans dirty? (Is there a positive correlation between spoonscore and equipment count)
+            # Viz - Scatter of pan-count to spoon score
+        # 2. What factor contributes the most to spoonscore - Linear Regression - Correlation matrix (check dependent), scatter plot (check assumptions), Bar chart (show relative impact)
+        # 3. Are vegan recipes more expensive? - Scatter
+    
+    #In addition to simply answering the question you should come up with at least 3 visualizations total
+
+
+
+
+# you also need to end up with your data inside an relational database. A local SQLite database would be sufficient.
+    #You should load as much of your data as possible into a relational database.
+
